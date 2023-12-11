040a5017ef65fa558ec8d46a567e8dc2412dfb0d
user <freedomseok@gmail.com>
2023-12-11 10:11:21 +0900

    231211 1교시

--- ch9_crawling1/Code09-02 aladin.py
+++ ch9_crawling1/Code09-02 aladin.py
@@ -1,7 +1,9 @@
 import bs4
 import urllib.request
 import csv
+
 # 알라딘  -> csv
+
 # <ul class="b-booklist">
     #   <li>
 	#     <div class="b-cover">
@@ -12,6 +14,7 @@ def getBookInfoImg(book_tag):
     img_tag_src = img_tag['src']
     
     return [img_tag_src]
+
 # <ul class="b-booklist">
     #   <li>
 	#     <div class="b-text">
@@ -29,12 +32,17 @@ def getBookInfoTxt(book_tag):
     bookName = book_tag.find("a").text
        
     return [bookName,authorName, price3]
+
+
+
 # 전역 변수부
 # url = "http://www.yes24.com/24/Category/Display/001001003022004?ParamSortTp=05&PageNumber="
 # pageNumber = 1
+
 # 전역 변수부
 # 해당 사이트의 하위 주소 부분 반드시 조사.
 # https://www.yes24.com/24/Category/Display/001001003022?ParamSortTp=05&PageNumber=2
+# md 추천 신간 -> 데이터 16개 정도 예제
 url = "https://www.aladin.co.kr/shop/wbrowse.aspx?CID=351"
 
 
@@ -48,7 +56,9 @@ txt_list = []
 result_list = []
 
 # 메인 코드부
-csvName = 'C:/TestPython/ch9_crawling1/aladinBook_231208.csv'
+# 수집 된 데이터를 -> csv 파일로 작성하는 초기 세팅. 
+csvName = '/Users/user/TESTPYTHON/ch9_crawling1/aladinBook_231211.csv'
+# with 구문은 , 파일의 입출력에서, 자동으로 close 할 때 많이 사용이 되는 형식이다. 
 with open(csvName, 'w', newline='',encoding="UTF-8") as csvFp:
     csvWriter = csv.writer(csvFp)
     csvWriter.writerow(['책이름', '저자', '가격', '책커버이미지'])
@@ -61,11 +71,15 @@ while True:
         htmlObject = urllib.request.urlopen(url)
         webPage = htmlObject.read()
         bsObject = bs4.BeautifulSoup(webPage, 'html.parser')
+
+
         tag = bsObject.find('ul', {'class': 'b-booklist'})
         # 구조 일반 이미지 : <div class="b-cover">,
         all_books_Img = tag.findAll('div', {'class': 'b-cover'})
+
         # 일반 텍스트  : <div class="b-text">
         all_books_Txt = tag.findAll('div', {'class': 'b-text'})
+
         # 이미지 주소만 가져오기. 
         for book in all_books_Img:
             bookImg = getBookInfoImg(book)
@@ -84,7 +98,7 @@ while True:
             # txt_list.append(bookAuthor)
             # bookPrice = bookTxt[2]
             # txt_list.append(bookPrice)
-
+        
         # img_list -> 사진 1장 url , 
         # txt_list -> [책제목,저자,가격]
         # result_list -> 
@@ -99,16 +113,24 @@ while True:
             # 사진 한장 뿐
             result_list.append(img_list[i])
             i = i+1
-
+                
 
         # for book in result_list:
         #     info_list = getBookInfo(book)
         print(f"result_list : {result_list}")
+
+        #4개씩 끊기 작업, 
+      
         # 쓰기 잠시 대기. -> 4개씩 끊어서 쓰기 작업. 
-        # with open(csvName, 'a', newline='',encoding="UTF-8") as csvFp:
-        #     csvWriter = csv.writer(csvFp)
-        #     csvWriter.writerow(result_list)
+        chunked_data = [result_list[i:i+4] for i in range(0,len(result_list),4)]
+        # chunked_data = [[책제목,저자,가격,책커버],[책제목2,저자2,가격2,책커버2],...]
+        for result in chunked_data:
+            with open(csvName, 'a', newline='',encoding="UTF-8") as csvFp:
+                csvWriter = csv.writer(csvFp)
+                # 한줄씩, csv 파일에 쓰기 작업한다. 
+                csvWriter.writerow(result)
 
     except:
         break
+
 print('Save. OK~')
